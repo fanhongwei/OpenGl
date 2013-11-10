890a532b423383c3f5c6cd338664b99ab05d09d9
fanhongwei <fanhongweiqq@gmail.com>
2013-11-10 19:59:26 +0800

    the end

--- 实验二：日地月模型.cpp
+++ 实验二：日地月模型.cpp
@@ -13,92 +13,121 @@
 
 void Initial()
 {
-	glEnable(GL_DEPTH_TEST);	// 启用深度测试
-	glFrontFace(GL_CCW);		// 指定逆时针绕法表示多边形正面
-
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f );	//背景为白色
+    glEnable(GL_DEPTH_TEST);	// 启用深度测试
+    glFrontFace(GL_CCW);		// 指定逆时针绕法表示多边形正面
+    glEnable(GL_CULL_FACE);     //启用剔除效果   
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f );	//背景为白色
 }
 
+void SetupRC(){
+
+    //设置光源LIGHT0的参数
+    GLfloat light_diffuse[]={1.0f,1.0f,1.0f,1.0f};
+    GLfloat light_ambient[]={0.0f,0.0f,0.0f,0.0f};
+    GLfloat light_specular[]={1.0f,1.0f,1.0f,1.0f};
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,light_diffuse);    //散射光
+    glLightfv(GL_LIGHT0,GL_AMBIENT,light_ambient);    //环境光
+    glLightfv(GL_LIGHT0,GL_SPECULAR,light_specular);  //镜面光
+
+    //是光源有效
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    //使用颜色材质
+    glEnable(GL_COLOR_MATERIAL);
+
+    //设置材料反射属性
+    glColorMaterial(GL_FRONT,GL_AMBIENT_AND_DIFFUSE);
+}
 void ChangeSize(int w, int h)
 {
-	if(h == 0)	h = 1;
+    if(h == 0)	h = 1;
 
-	// 设置视区尺寸
+    // 设置视区尺寸
     glViewport(0, 0, w, h);
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
-	// 设置修剪空间
-	GLfloat fAspect;
-	fAspect = (float)w/(float)h;
-	gluPerspective(45.0, fAspect, 1.0, 500.0);
-/*
-	    if (w <= h) 
-			glOrtho (-nRange, nRange, nRange*h/w, -nRange*h/w, -nRange*2.0f, nRange*2.0f);
-		else 
-			glOrtho (-nRange*w/h, nRange*w/h, nRange, -nRange, -nRange*2.0f, nRange*2.0f);
-*/
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    // 设置修剪空间
+    GLfloat fAspect;
+    fAspect = (float)w/(float)h;
+    gluPerspective(45.0, fAspect, 1.0, 500.0);
+    /*
+       if (w <= h) 
+       glOrtho (-nRange, nRange, nRange*h/w, -nRange*h/w, -nRange*2.0f, nRange*2.0f);
+       else 
+       glOrtho (-nRange*w/h, nRange*w/h, nRange, -nRange, -nRange*2.0f, nRange*2.0f);
+       */
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 
 void RenderScene(void)
 {
-	// 绕原子核旋转的角度
-	static float fElect1 = 0.0f,fElect2=0.0f;
+    // 绕原子核旋转的角度
+    static float fElect1 = 0.0f,fElect2=0.0f;
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    GLfloat position[]={0.0f,0.0f,0.0f,1.0f};
 
-	// 重置模型视图矩阵
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    //设置光源的位置
+    //glLightfv(GL_LIGHT0,GL_POSITION,position);
 
-	//将图形沿z轴负向移动
-	glTranslatef(0.0f, 0.0f, -250.0f);	
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// 绘制红色的太阳
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glutSolidSphere(40, 60, 60);
-  glRotatef(fElect1,0.0f,1.0f,0.0f);
-  glTranslatef(0.0f,0.0f,0.0f);
-	// 当前绘制颜色变为黄色
-	glColor3f(0.0f, 0.0f, 0.0f);
+    // 重置模型视图矩阵
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glPushMatrix();
 
-	//绘制蓝色的地球
-	//保存当前的模型视图矩阵
-	glPushMatrix();
-  glColor3f(0.0f,0.0f,1.0f);
-	glRotatef(fElect1, 0.0f, 1.0f, 0.0f);//绕y轴旋转一定的角度
-	glTranslatef(130.0f, 0.0f, 0.0f);//平移一段距离
-	glutSolidSphere(10.0f, 60, 60);//画出电子
+    //向远移动场景
+    glTranslatef(0.0f,0.0f,-300.0f);
+
+    // 绘制红色的太阳
+    glDisable(GL_LIGHTING);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glutSolidSphere(40, 60, 60);
+    glRotatef(fElect1,0.0f,1.0f,0.0f);
+    glTranslatef(0.0f,0.0f,0.0f);
+    // 当前绘制颜色变为黄色
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glEnable(GL_LIGHTING);
+    glLightfv(GL_LIGHT0,GL_POSITION,position);
+
+    //绘制蓝色的地球
+    //保存当前的模型视图矩阵
+    glPushMatrix();
+    glColor3f(0.0f,0.0f,1.0f);
+    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);//绕y轴旋转一定的角度
+    glTranslatef(130.0f, 0.0f, 0.0f);//平移一段距离
+    glutSolidSphere(15.0f, 60, 60);//画出电子
 
 
-	// 绘制黄色的月亮
-  glPushMatrix();
-  glColor3f(1.0f,1.0f,0.0f);
-	glRotatef(fElect2, 0.0f, 1.0f, 0.0f);
-	glTranslatef(20.0f, 0.0f, 0.0f);
-	glutSolidSphere(6.0f, 60,60);
-	glPopMatrix();
+    // 绘制黄色的月亮
+    glPushMatrix();
+    glColor3f(1.0f,1.0f,0.0f);
+    glRotatef(fElect2, 0.0f, 1.0f, 0.0f);
+    glTranslatef(20.0f, 0.0f, 0.0f);
+    glutSolidSphere(8.0f, 60,60);
+    glPopMatrix();
 
-	// 第三个电子
-	//glPushMatrix();
-	//glRotatef(-45.0f,0.0f, 0.0f, 1.0f);
-	//glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-	//glTranslatef(0.0f, 0.0f, 60.0f);
-	//glutSolidSphere(6.0f, 15, 15);
-	//glPopMatrix();
+    // 第三个电子
+    //glPushMatrix();
+    //glRotatef(-45.0f,0.0f, 0.0f, 1.0f);
+    //glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+    //glTranslatef(0.0f, 0.0f, 60.0f);
+    //glutSolidSphere(6.0f, 15, 15);
+    //glPopMatrix();
 
-	// 增加旋转步长
-	fElect1 += 2.0f;
-	if(fElect1 > 360.0f)	fElect1 = 10.0f;
-  fElect2+=20.0f;
-  if(fElect2>360.0f){
-   fElect2=10.0f;
-  }
-	glutSwapBuffers();
+    // 增加旋转步长
+    fElect1 += 2.0f;
+    if(fElect1 > 360.0f)	fElect1 = 10.0f;
+    fElect2+=20.0f;
+    if(fElect2>360.0f){
+        fElect2=10.0f;
+    }
+    glutSwapBuffers();
 }
 
 void TimerFunc(int value)
@@ -109,16 +138,18 @@ void TimerFunc(int value)
 
 int main(int argc, char* argv[])
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowSize(1200,700);
-	glutCreateWindow("日地月模型");
-	glutReshapeFunc(ChangeSize);
-	glutDisplayFunc(RenderScene);
-  glutTimerFunc(500, TimerFunc, 1);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(1200,700);
+    glutCreateWindow("日地月模型");
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    glutTimerFunc(500, TimerFunc, 1);
 
-	Initial();
-	glutMainLoop();
+    SetupRC();
 
-	return 0;
+    Initial();
+    glutMainLoop();
+
+    return 0;
 }
